cb604fe60f28725a79e7027e7d94ed40d728a9c7
Raynerenyar <raynerlim86@gmail.com>
2023-01-03 22:00:06 +0800

    complete

--- src/main/java/sg/edu/nus/iss/app/workshop11/Workshop11Application.java
+++ src/main/java/sg/edu/nus/iss/app/workshop11/Workshop11Application.java
@@ -1,13 +1,55 @@
 package sg.edu.nus.iss.app.workshop11;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.slf4j.LoggerFactory;
+import java.util.Collections;
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class Workshop11Application {
+	private static final Logger logger = LoggerFactory.getLogger(Workshop11Application.class);
+
+	private static String portNumber = null;
+	private static final String DEFAULT_PORT = "8080";
 
 	public static void main(String[] args) {
-		SpringApplication.run(Workshop11Application.class, args);
-	}
 
+		for (String arg : args) {
+			logger.debug("arg given: " + arg);
+			if (arg.contains("--port=")) {
+				portNumber = arg.substring(arg.length() - 4, arg.length());
+				logger.debug("portNumber: " + portNumber);
+			}
+		}
+
+		// if portNumber is null, assign port from system env
+		if (portNumber == null) {
+			portNumber = System.getenv("APP_PORT");
+			logger.debug("Sys ENV portNumber : " + portNumber);
+		}
+
+		// if portNumber (system env port is null), assign hardcoded default port
+		if (portNumber == null) {
+			portNumber = DEFAULT_PORT;
+		}
+
+		// SpringApplication.run(Workshop11Application.class, args);
+		SpringApplication app = new SpringApplication(Workshop11Application.class);
+
+		// set port into spring
+		// spring properties are found in
+		// https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html
+		app.setDefaultProperties(Collections.singletonMap("server.port", portNumber));
+
+		System.out.printf("%s\n", args[0]);
+		System.out.println("Application started on: " + portNumber);
+
+		// run
+		// pass args as argument into run()
+		app.run(args);
+		// this line and the above line will only run after spring runs main twice
+		// during the 2 main method calls, the run() and lines after that would not run
+		System.out.println("RUNNING");
+	}
 }
